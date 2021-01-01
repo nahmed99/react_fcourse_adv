b0256eb131cbc0e72ffb4a50de305affe250f7d8
nahmed99 <ahmed_nawaz@hotmail.com>
2021-01-01 20:08:41 +0000

    useReducer started

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 // As the following 'module' has been exported as default, we can name it anything we want. We are going to name it as stated immediately after 'import'.
-import Setup from './tutorial/5-useRef/setup/1-useRef-basics';
+import Setup from './tutorial/6-useReducer/setup';
 
 function App() {
   return (
